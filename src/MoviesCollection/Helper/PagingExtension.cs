57339a7b16efce79e469226aae45ec8a7cb30a47
hoantc <hoantc@mti-tech.vn>
2020-12-11 17:48:09 +0700

    add pager

---
+++ src/MoviesCollection/Helper/PagingExtension.cs
@@ -0,0 +1,19 @@
+﻿using MoviesCollection.Models;
+
+namespace MoviesCollection.Helper
+{
+    public static class PagingExtension
+    {
+        public static PagedResult<MovieDetailModel> ToPagedResult(this MovieResponseModel moviesResponseModel)
+        {
+            return new PagedResult<MovieDetailModel>()
+            {
+                PageCount = moviesResponseModel.total_pages,
+                RowCount = moviesResponseModel.total_results,
+                CurrentPage = moviesResponseModel.page,
+                Results = moviesResponseModel.results,
+                PageSize = 20
+            };
+        }
+    }
+}
---
+++ src/MoviesCollection/Models/PagedResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MoviesCollection.Models
+{
+    public abstract class PagedResultBase
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+    }
+ 
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public IList<T> Results { get; set; }
+ 
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
